16401b46e705bf6714c58cf8881168af74a86388
이동혁 <ehdgur920907@tmon.co.kr>
2019-12-27 12:40:05 +0900

    Add Counter logic

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <ToDoList/>
+    <Counter/>
   </div>
 </template>
 
 <script>
 import ToDoList from "./components/ToDoList";
+import Counter from "./components/Counter";
 
 export default {
   name: "app",
   components: {
-    ToDoList
+    ToDoList,
+    Counter
   }
 };
 </script>
---
+++ src/components/Counter/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="counter"></div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  components: {},
+  data() {
+    return { counter: 0 };
+  },
+  methods: {
+    increment() {
+      this.counter += 1;
+    }
+  }
+};
+</script>
+
