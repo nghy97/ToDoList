0792aed9229705cb59a0173b0a21837c402d19a2
DONGHYUK <ehdgur920907@gmail.com>
2020-01-02 14:15:13 +0900

    Add v-else

--- src/components/ToDoList/Header.vue
+++ src/components/ToDoList/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <h1 v-if="isVisible">To Do List</h1>
+    <h1 v-else>안 보이지?</h1>
   </header>
 </template>
 
@@ -8,6 +9,6 @@
 export default {
   data: () => ({
     isVisible: true
-  }),
-}
+  })
+};
 </script>
